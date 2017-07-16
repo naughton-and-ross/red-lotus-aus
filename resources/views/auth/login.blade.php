950bf142693197fb5ed8dbc0a7d637dc0ed0e1f4
William Naughton-Gravette <william@willng.me>
2017-07-16 17:01:56 +1000

    oops

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,3 @@
-@section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
@@ -50,4 +49,3 @@
         </div>
     </div>
 </div>
-@endsection
