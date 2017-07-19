298f3f415a31aaa7975a48e3c57aab1bbae538fe
William Naughton-Gravette <william@willng.me>
2017-07-19 23:40:49 +1000

    fix email template

--- resources/views/mail/form.blade.php
+++ resources/views/mail/form.blade.php
@@ -5,6 +5,6 @@
         <title></title>
     </head>
     <body>
-        <b>{{ $details->name }}</b> sent an enquiry to <b>@if ($catagory = "band") Red Lotus @elseif ($catagory = "studio") Studio Equilibrium @endif</b> regarding <b>{{ $details->subject }}</b>. This is what they said: <b>{{ $details->text }}</b>. Reach them at: <b>{{ $details->contact }}</b>
+        <b>{{ $details->name }}</b> sent an enquiry to <b>@if ($catagory == "band") Red Lotus @elseif ($catagory == "studio") Studio Equilibrium @endif</b> regarding <b>{{ $details->subject }}</b>. This is what they said: <b>{{ $details->text }}</b>. Reach them at: <b>{{ $details->contact }}</b>
     </body>
 </html>
