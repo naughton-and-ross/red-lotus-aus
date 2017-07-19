f981fd4a31e0701efbcd9887b93bb1caac4b7c70
William Naughton-Gravette <william@willng.me>
2017-07-19 23:12:31 +1000

    test larval mail

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Mail\ContactSent;
+use App\Mail\NewBandInquiry;
 use Illuminate\Support\Facades\Mail;
 
 
@@ -16,33 +16,37 @@ class ContactController extends Controller
 
     public function sendEmail(Request $details)
     {
-        $name = $details->name;
-        $contact = $details->contact;
-        $type = $details->subject;
-        $comments = $details->text;
 
-        $message = '<html><body>';
-        $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
-        $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . $name . "</td></tr>";
-        $message .= "<tr><td><strong>Contact:</strong> </td><td>" . $contact . "</td></tr>";
-        $message .= "<tr><td><strong>Subject:</strong> </td><td>" . $type . "</td></tr>";
-        $message .= "<tr><td><strong>Other Comments:</strong> </td><td>" . $comments . "</td></tr>";
-        $message .= "</table>";
-        $message .= "</body></html>";
+        Mail::to('william.gravette@gmail.com')->send(new NewBandInquiry($details));
 
-        $to = 'william.gravette@gmail.com';
 
-        $subject = 'Inquiry for Red Lotus';
-
-        $headers = "From: " . "Red Lotus Website" . "\r\n";
-        $headers .= "MIME-Version: 1.0\r\n";
-        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-        if (mail($to, $subject, $message, $headers)) {
-            return redirect('contact')->with('status', 'Your message has been sent!');
-        } else {
-            return redirect('contact');
-        }
+        // $name = $details->name;
+        // $contact = $details->contact;
+        // $type = $details->subject;
+        // $comments = $details->text;
+        //
+        // $message = '<html><body>';
+        // $message .= '<table rules="all" style="border-color: #666;" cellpadding="10">';
+        // $message .= "<tr style='background: #eee;'><td><strong>Name:</strong> </td><td>" . $name . "</td></tr>";
+        // $message .= "<tr><td><strong>Contact:</strong> </td><td>" . $contact . "</td></tr>";
+        // $message .= "<tr><td><strong>Subject:</strong> </td><td>" . $type . "</td></tr>";
+        // $message .= "<tr><td><strong>Other Comments:</strong> </td><td>" . $comments . "</td></tr>";
+        // $message .= "</table>";
+        // $message .= "</body></html>";
+        //
+        // $to = 'william.gravette@gmail.com';
+        //
+        // $subject = 'Inquiry for Red Lotus';
+        //
+        // $headers = "From: " . "Red Lotus Website" . "\r\n";
+        // $headers .= "MIME-Version: 1.0\r\n";
+        // $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        //
+        // if (mail($to, $subject, $message, $headers)) {
+        //     return redirect('contact')->with('status', 'Your message has been sent!');
+        // } else {
+        //     return redirect('contact');
+        // }
 
     }
 }
--- resources/views/mail/form.blade.php
+++ resources/views/mail/form.blade.php
@@ -5,6 +5,6 @@
         <title></title>
     </head>
     <body>
-        {{ $details }}
+        {{ $details->name }}
     </body>
 </html>
