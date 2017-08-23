434710d692296444525e4f76260811dd9bd5d940
William Naughton-Gravette <william@willng.me>
2017-08-24 08:15:24 +1000

    iTunes preorder

--- resources/views/music.blade.php
+++ resources/views/music.blade.php
@@ -5,7 +5,7 @@
         <div class="l-box">
             <h1>"ILLUMINATE" OUT 21 SEPTEMBER</h1>
             <p>
-                Until then, pre-order on <a class="body" href="">iTunes</a>, <a class="body" href="">GooglePlay</a>, or on our <a class="body" href="http://redlotusaus.bigcartel.com/">Store</a>
+                Until then, pre-order on <a class="body" href="https://itunes.apple.com/au/album/illuminate/id1264901265">iTunes</a>, <a class="body" href="">GooglePlay</a>, or on our <a class="body" href="http://redlotusaus.bigcartel.com/">Store</a>
             </p>
         </div>
     </div>
