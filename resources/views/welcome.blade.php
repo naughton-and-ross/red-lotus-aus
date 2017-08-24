e61a5445c055e29ab963579018826b9cc660d011
William‌‌ Naughton-Gravette <william@willng.me>
2017-08-24 11:12:08 +1000

    New YouTube links

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
 </div>
 <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
     <div class="embed-container" id="mobilePlayer">
-        <iframe src='https://www.youtube.com/embed/cFWeW1pBYkM' frameborder='0' allowfullscreen></iframe></div>
+        <iframe src='https://www.youtube.com/embed/IyFFtw8uu_w' frameborder='0' allowfullscreen></iframe></div>
     </div>
 <!--
 <div class="pure-g">
