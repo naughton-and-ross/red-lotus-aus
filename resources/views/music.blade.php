7bf48fcb29dbc8e4efab95585ac09d15bab2c160
William‌‌ Naughton-Gravette <william@willng.me>
2017-08-24 11:10:28 +1000

    Google play link

--- resources/views/music.blade.php
+++ resources/views/music.blade.php
@@ -5,7 +5,7 @@
         <div class="l-box">
             <h1>"ILLUMINATE" OUT 21 SEPTEMBER</h1>
             <p>
-                Until then, pre-order on <a class="body" href="https://itunes.apple.com/au/album/illuminate/id1264901265">iTunes</a>, <a class="body" href="">GooglePlay</a>, or on our <a class="body" href="http://redlotusaus.bigcartel.com/">Store</a>
+                Until then, pre-order on <a class="body" href="https://itunes.apple.com/au/album/illuminate/id1264901265">iTunes</a>, <a class="body" href="https://play.google.com/store/music/album/Red_Lotus_Illuminate?id=Befd7p4urllsawnunwrrexsb3ii">GooglePlay</a>, or on our <a class="body" href="http://redlotusaus.bigcartel.com/">Store</a>
             </p>
         </div>
     </div>
